73a4a3566f612d81da0d18c71b3c980c0c2c07b3
vamosia <butarian@gmail.com>
2017-08-24 23:25:05 +0200

    added firehose_get

--- cbio.fix.mutation.pl
+++ cbio.fix.mutation.pl
@@ -31,268 +31,25 @@ my %id = ( 'cbio' => ['entrez_gene_id',
 		       'End_Position',
 		       'HGVSp_Short|MA:protein.change|amino_acid_change|AAChange']);
 
-# Get Study
-my $cbio_dat;
-my %entrez;
+
 my $study = `pwd`;
 my $cnt = 0;
 chomp $study;
+
 $study =~ s/.*\/tcga\/(.*?)\/data_mutations_extended.BAK/$1/;
 
-debug( -id => 'INFO',
-       -val => 'Loading Cbio Data' );
+debug( -id => 'INFO', -val => 'Loading Cbio Data' );
+
+my ($cbio_dat, $cbio_entrez) = load_cbio( -f => 'data_mutations_extended.txt.cbio' );
+
+debug( -id => 'INFO', -val => 'Checking for consistency - data_mutation_extended.txt.fix.entrez.chr' );
+
+my $dup = check_data( -f => 'data_mutations_extended.txt.fix.entrez.chr' );
+
+debug( -id => 'INFO', -val => 'Running VLOOKUP' );
 
 
-$cbio_dat = load_data_cbio();
-
-debug( -id => 'INFO',
-       -val => 'Processing Duplicate - data_mutation_extended.txt.fix.entrez.chr' );
-
-my $dup = duplicate_analysis();
-
-debug( -id => 'INFO',
-       -val => 'Proccesing VLOOKUP' );
-
-
-open( IN, "< data_mutations_extended.txt.fix.entrez.chr" ) or die "$!\n";
-open( OUT,"> data_mutations_extended.txt.fix.entrez.chr.refvar" ) or die "$!\n";
-open( NOTFIX,"> data_mutations_extended.txt.fix.entrez.chr.refvar.notfix" ) or die "$!\n";
-
-
-    
-my @header = ();
-my %fix;
-$cnt = 0;
-
-while( <IN> ) {
-
-    chomp $_;
-
-    if( $_ =~ /^Hugo_Symbol/ ) {
-	@header = split( /\t/, $_ );
-
-	# This is required as we're adding a new tag
-	push( @header, 'AAChange_cbio' );
-	
-	print OUT join( "\t", @header ) . "\n";
-	next;
-    }
-    my @line = split( /\t/, $_ );
-    
-    my %line;
-
-    @line{ @header } = @line;
-        
-    my @id_val;
-
-    
-    foreach ( @{ $id{ 'local'} } ) {
-
-	# AA can be mapped to 'HGVSp_Short|MA:protein.change|amino_acid_change|AAChange']);
-	# check to see which one maps
-	if( $_ =~ /\|/ ) {
-	    
-	    my @tag = split( '\|', $_ );
-	    
-	    foreach( @tag ) {
-		
-		next unless( exists( $line{ $_ } ) && 
-			     defined $line{ $_ } );
-
-		# Replace p.Y282K to Y282K
-		$line{ $_ } =~ s/p\.//;
-
-		# Relace Y219X to Y219*
-		$line{ $_ } =~ s/(\d)X$/$1*/g if( $_ eq 'AAChange' && $line{ $_ } =~ /X$/);
-		
-		my $id = join( "+", @id_val ) . '+';
-		$id .= $line{ $_ } || "";
-		
-		if (exists $cbio_dat->{ $id } ) {
-		    
-		    push( @id_val, $line{ $_ } );
-
-		    # Keep tag of this correct AA, also need to add to the header so it prints correctly on the files
-		    $line{ 'AAChange_cbio' } = $line{ $_ };
-		    
-		    goto AAFOUND;
-		    last;
-		} 
-	    }
-	    
-	} else {
-	    push( @id_val, $line{ $_ } );
-	    
-	}
-
-      AAFOUND:
-	
-    }
-    
-    my $id = join( "+", @id_val );
-    
-    if (exists $cbio_dat->{ $id } ) {
-	
-	my %map = ( 'Reference_Allele' => 'reference_allele',
-		    'Tumor_Seq_Allele1' => 'variant_allele',
-		    't_depth' => 'reference_read_count_tumor+variant_read_count_tumor',
-		    't_ref_count' => 'reference_read_count_tumor',
-		    't_alt_count' => 'variant_read_count_tumor',
-		    'n_depth' => 'reference_read_count_normal+variant_read_count_normal',
-		    'n_ref_count' => 'reference_read_count_normal',
-		    'n_alt_count' => 'variant_read_count_normal' );
-
-	my $is_dup = $dup->{ $id }{ cnt } || 0;
-
-	
-	while ( my( $id_loc, $id_cbio) = each (%map)) {
-	    
-	    # if its a duplicate, only change the reference. Becase A32K can be T>A or T>G
-	    next if( $is_dup > 1 && ($id_cbio =~ /count/ or $id_cbio =~ /variant/) );
-
-	    # TODO : if its a duplicate, check whats different, what is the same
-    	    
-	    # skip if id doesn't exists in local file (i.e see brca file the header is different)
-	    next if( ! exists $line{ $id_loc } );
-	    
-	    my $old_val = $line{ $id_loc };
-
-	    my $new_val;
-
-	    # if + is entencountered, we need to add these two values
-	    if( $id_cbio =~ /\+/ ) {
-		
-		my @id_line = split( /\+/, $id_cbio );
-		
-		foreach( @id_line ) {
-
-		    if( $cbio_dat->{ $id }{ $_ } eq 'NA' || $cbio_dat->{ $id }{ $_ } eq '') {
-			$new_val = $cbio_dat->{ $id }{ $_ };
-			last;
-			
-		    } else {
-			$new_val += $cbio_dat->{ $id }{ $_ };
-		    }
-		    
-		}
-	    } else {
-		$new_val = $cbio_dat->{ $id }{ $id_cbio };
-	    }
-	    
-	    print "[INFO] $id_loc : $old_val >  $new_val\n" if( $options{ -d } == 1 );
-
-	    next if $new_val eq 'NA';
-   
-	    # Replace old values
-	    $line{ $id_loc } = $new_val;
-	}
-	
-	$fix{ yes }++;
-	
-    } else {
-
-	# This section if for those entry not in CBIO
-	
-	my $a = $line{ 'HGVSp_Short' } || "-";
-	my $b = $line{ 'MA:protein.change' } || "-";
-	my $c = $line{ 'amino_acid_change' } || "-";
-	my $d = $line{ 'AAChange' } || "-";
-	my $varClass = $line{ 'Variant_Classification' };
-	
-	my %ignore = qw(Silent 1 Intron 1 3'UTR 1 3'Flank 1 5'UTR 1 5'Flank 1 IGR 1 RNA 1);
-
-	
-	if( exists $ignore{ $varClass } ) {
-	    # variantClass ignored
-	    $fix{ no_varClass }++;
-	    print NOTFIX "no_varClass | $varClass | $line{ Hugo_Symbol } | $id ($a,$b,$c,$d)\n";
-
-	} elsif ( ! exists $entrez{ $line{ 'Entrez_Gene_Id' } } ) {
-	    # this entrez_id, doesn't exits in the CBIO file
-	    $fix{ no_entrez }++;
-	    print NOTFIX "no_entrez | $varClass | $line{ Hugo_Symbol } | $id ($a,$b,$c,$d)\n";
-	    
-	    # We actually need to skip this so its not displayed in the web
-	    next;
-	}
-    }
-
-    # Check to see if we can restore the allele counts
-    # t_depth = t_ref_count + t_alt_count
-	
-    if( ! $line{ 't_depth' } =~ /\d/ && 
-	$line{ 't_ref_count' } =~ /\d/ && 
-	$line{ 't_alt_count' } =~ /\d/ ) {
-	
-	$line{ 't_depth' } = $line{ 't_ref_count' } + $line{ 't_alt_count' };
-	
-    }
-    if( ! $line{ 't_ref_count' } =~ /\d/ && 
-	$line{ 't_depth' } =~ /\d/ && 
-	$line{ 't_alt_count' } =~ /\d/ ) {
-	$line{ 't_ref_count' } = $line{ 't_depth'} - $line{ 't_alt_count' };
-	
-    }
-    if( ! $line{ 't_alt_count' } =~ /\d/  &&
-	$line{ 't_depth' } =~ /\d/ && $line{ 't_ref_count' } =~ /\d/ ) {
-	$line{ 't_alt_count' } = $line{ 't_depth' } - $line{ 't_ref_count' };
-    }
-    
-    # n_depth = n_ref_count + n_alt_count
-    if( ! $line{ 'n_depth' } =~  /\d/ && 
-	$line{ 'n_ref_count' } =~ /\d/ && $line{ 'n_alt_count' } =~ /\d/) {
-	$line{ 'n_depth' } = $line{ 'n_ref_count' } + $line{ 'n_alt_count' };
-    }
-    if( ! $line{ 'n_ref_count' } =~ /\d/ &&
-	$line{ 'n_depth' } =~ /\d/ && $line{ 'n_alt_count' } =~ /\d/ ) {
-	$line{ 'n_ref_count' } = $line{ 'n_depth'} - $line{ 'n_alt_count'  };
-    }
-
-    if( ! $line{ 'n_alt_count' } =~ /\d/ &&
-	$line{ 'n_depth' } =~ /\d/ && $line{ 'n_ref_count' } =~ /\d/ ) {
-	$line{ 'n_alt_count' } = $line{ 'n_depth' } - $line{ 'n_ref_count' };
-    }
-    
-    my $line = "";
-    
-    foreach( @header ) {
-	
-	$line .= $line{ $_ } || "";
-	$line .= "\t";
-	
-    }
-    
-    chop $line;
-    print OUT $line,"\n";
-    $fix{ total }++;
-
-    $cnt++;
-    print $cnt,"\r" if( $options{ -d } == 1 );
-}
-print "\n" if( $options{ -d } == 1 );
-
-my $yes = $fix{ 'yes' } || 0;
-my $no_varClass = $fix{ 'no_varClass' } || 0;
-my $no_entrez  = $fix{ 'no_entrez' } || 0;
-my $no_total = $no_varClass + $no_entrez;
-my $total = $fix{ 'total' } || 0;
-
-debug( -id => "INFO",
-       -val => "Fixing : refvar ( $yes / $total )" );
-
-debug( -id => "INFO",
-       -val => "Fixing : refvar - Ignored Total ( $no_total / $total )" );
-
-
-debug( -id => "INFO",
-       -val => "  -> Ignored Variant Classification ( $no_varClass / $total )" );
-
-debug( -id => "INFO",
-       -val => "  -> Ignored Entrez not found in cBio ( $no_entrez / $total )" );
- 
-close( IN );
-close( OUT );
-close( NOTFIX );
+vlookup();
 
 
 ################################################################################################################################
@@ -301,12 +58,14 @@ close( NOTFIX );
 ###
 ################################################################################################################################
 
-sub load_data_cbio {
-    
-    my ($cbio_head, $cbio_mut) = read_file( -f => 'data_mutations_extended.txt.cbio' );
-    
-    my %cbio_dat;
+sub load_cbio {
 
+    my %param = @_;
+
+    my ($cbio_head, $cbio_mut) = read_file( -f => $param{ -f } );
+    
+    my (%cbio_dat, %cbio_entrez);
+    
     # FIX REF / COUNT
     foreach my $line ( @{ $cbio_mut } ) {
 	
@@ -346,20 +105,28 @@ sub load_data_cbio {
 		   -val => "CBIOPORTAL $study | $cbio_dat{ $id }{ gene_symbol } | $id" );
 	}
 
-	$entrez{ $line->{ 'entrez_gene_id' } }++;	
+	# check to see if all the entrez from cbio was found in local data
+	$cbio_entrez{ $line-> { 'entrez_gene_id' } }++
+	
+	
+	
     }
-    return( \%cbio_dat );
+    return( \%cbio_dat, \%cbio_entrez );
 }
 
 
-sub duplicate_analysis {
+sub check_data {
 
+    my %param = @_;
+    
     # COUNT DUPLICATE
-    open( IN, "< data_mutations_extended.txt.fix.entrez.chr" ) or die "$!\n";
+    open( IN, "< $param{ -f }" ) or die "$!\n";
 
     my @header;
     my %dup;
     my %dup_remove;
+    my %loc_entrez;
+    
     while( <IN> ) {
 	
 	chomp $_;
@@ -377,8 +144,7 @@ sub duplicate_analysis {
 
 	@line{ @header } = @line;
         
-	my @id_val;
-
+	my @id_val;	
 	
 	foreach ( @{ $id{ 'local'} } ) {
 
@@ -419,14 +185,20 @@ sub duplicate_analysis {
 
 	$dup{ $id }{ cnt }++;
 	$dup{ $id }{ 'type' }{ $line{ 'Variant_Classification' } }++;
-	my @a;
-	# push( @{ $dup{ $id }{ 'data' } }, \%line );
-
-    
+	
+	# Count the number of times the local entrez exists
+	$loc_entrez{ $line{ 'Entrez_Gene_Id' } }++;
     }
+    
     close( IN );
-    
-    
+
+    # if any of the cbio entrez cannot be found on the local file, print warning
+    foreach( keys %{ $cbio_entrez } ) {
+	
+	unless( exists $loc_entrez{ $_ } ) {
+	    debug( -id => 'INFO', -val => "  -> CBio Entrez not found in local : $_" );
+	}
+    }
 
     # Message for duplicate id
     my $dup_cnt_NA = 0;
@@ -473,3 +245,254 @@ sub duplicate_analysis {
     return( \%dup );
 }
 
+
+sub vlookup {
+
+    open( IN, "< data_mutations_extended.txt.fix.entrez.chr" ) or die "$!\n";
+    open( OUT,"> data_mutations_extended.txt.fix.entrez.chr.refvar" ) or die "$!\n";
+    open( NOTFIX,"> data_mutations_extended.txt.fix.entrez.chr.refvar.notfix" ) or die "$!\n";
+    
+    my @header = ();
+
+    my %fix;
+    $cnt = 0;
+
+    while( <IN> ) {
+
+	chomp $_;
+
+	if( $_ =~ /^Hugo_Symbol/ ) {
+	    @header = split( /\t/, $_ );
+
+	    # This is required as we're adding a new tag
+	    push( @header, 'AAChange_cbio' );
+	    
+	    print OUT join( "\t", @header ) . "\n";
+	    next;
+	}
+	my @line = split( /\t/, $_ );
+	
+	my %line;
+
+	@line{ @header } = @line;
+        
+	my @id_val;
+	my $entrez = $line{ 'Entrez_Gene_Id' };
+
+	
+	foreach ( @{ $id{ 'local'} } ) {
+
+	    # AA can be mapped to 'HGVSp_Short|MA:protein.change|amino_acid_change|AAChange']);
+	    # check to see which one maps
+	    if( $_ =~ /\|/ ) {
+		
+		my @tag = split( '\|', $_ );
+		
+		foreach( @tag ) {
+		    
+		    next unless( exists( $line{ $_ } ) && 
+				 defined $line{ $_ } );
+
+		    # Replace p.Y282K to Y282K
+		    $line{ $_ } =~ s/p\.//;
+
+		    # Relace Y219X to Y219*
+		    $line{ $_ } =~ s/(\d)X$/$1*/g if( $_ eq 'AAChange' && $line{ $_ } =~ /X$/);
+		    
+		    my $id = join( "+", @id_val ) . '+';
+		    $id .= $line{ $_ } || "";
+		    
+		    if (exists $cbio_dat->{ $id } ) {
+			
+			push( @id_val, $line{ $_ } );
+
+			# Keep tag of this correct AA, also need to add to the header so it prints correctly on the files
+			$line{ 'AAChange_cbio' } = $line{ $_ };
+			
+			goto AAFOUND;
+			last;
+		    } 
+		}
+		
+	    } else {
+		push( @id_val, $line{ $_ } );
+		
+	    }
+
+	  AAFOUND:
+	    
+	}
+	
+	my $id = join( "+", @id_val );
+	
+	if (exists $cbio_dat->{ $id } ) {
+	    
+	    my %map = ( 'Reference_Allele' => 'reference_allele',
+			'Tumor_Seq_Allele1' => 'variant_allele',
+			't_depth' => 'reference_read_count_tumor+variant_read_count_tumor',
+			't_ref_count' => 'reference_read_count_tumor',
+			't_alt_count' => 'variant_read_count_tumor',
+			'n_depth' => 'reference_read_count_normal+variant_read_count_normal',
+			'n_ref_count' => 'reference_read_count_normal',
+			'n_alt_count' => 'variant_read_count_normal' );
+
+	    my $is_dup = $dup->{ $id }{ cnt } || 0;
+
+	    
+	    while ( my( $id_loc, $id_cbio) = each (%map)) {
+		
+		# if its a duplicate, only change the reference. Becase A32K can be T>A or T>G
+		next if( $is_dup > 1 && ($id_cbio =~ /count/ or $id_cbio =~ /variant/) );
+
+		# TODO : if its a duplicate, check whats different, what is the same
+		
+		# skip if id doesn't exists in local file (i.e see brca file the header is different)
+		next if( ! exists $line{ $id_loc } );
+		
+		my $old_val = $line{ $id_loc };
+
+		my $new_val;
+
+		# if + is entencountered, we need to add these two values
+		if( $id_cbio =~ /\+/ ) {
+		    
+		    my @id_line = split( /\+/, $id_cbio );
+		    
+		    foreach( @id_line ) {
+
+			if( $cbio_dat->{ $id }{ $_ } eq 'NA' || $cbio_dat->{ $id }{ $_ } eq '') {
+			    $new_val = $cbio_dat->{ $id }{ $_ };
+			    last;
+			    
+			} else {
+			    $new_val += $cbio_dat->{ $id }{ $_ };
+			}
+		    }
+		    
+		} else {
+		    $new_val = $cbio_dat->{ $id }{ $id_cbio };
+		}
+
+		print "[INFO] $id_loc : $old_val >  $new_val\n" if( $options{ -d } == 1 );
+
+		# Allele counts does not accept NA, will not render correctly
+		next if $new_val eq 'NA';
+		
+		# Replace old values
+		$line{ $id_loc } = $new_val;
+	    }
+	    
+	    $fix{ yes }++;
+	    
+	} else {
+
+	    # This section if for those entry not in CBIO
+	    
+	    my $a = $line{ 'HGVSp_Short' } || "-";
+	    my $b = $line{ 'MA:protein.change' } || "-";
+	    my $c = $line{ 'amino_acid_change' } || "-";
+	    my $d = $line{ 'AAChange' } || "-";
+	    my $varClass = $line{ 'Variant_Classification' };
+	    
+	    my %ignore = qw(Silent 1 Intron 1 3'UTR 1 3'Flank 1 5'UTR 1 5'Flank 1 IGR 1 RNA 1);
+
+	    
+	    if( exists $ignore{ $varClass } ) {
+		# variantClass ignored
+		$fix{ no_varClass }++;
+		print NOTFIX "no_varClass (included) | $varClass | $line{ Hugo_Symbol } | $id ($a,$b,$c,$d)\n";
+		
+	    } elsif ( ! exists $cbio_entrez->{ $line{ 'Entrez_Gene_Id' } } ) {
+		# this entrez_id, doesn't exits in the CBIO file
+		$fix{ no_entrez }++;
+		print NOTFIX "no_entrez (excluded) | $varClass | $line{ Hugo_Symbol } | $id ($a,$b,$c,$d)\n";
+		
+		# We actually need to skip this so its not displayed in the web
+		# next;
+	    } else {
+
+		# this entrez_id, doesn't exits in the CBIO file
+		$fix{ no_entrez }++;
+		print NOTFIX "ID does not exists | $varClass | $line{ Hugo_Symbol } | $id ($a,$b,$c,$d)\n";
+	    }
+	}
+	
+	# Check to see if we can restore the allele counts
+	# t_depth = t_ref_count + t_alt_count
+	
+	if( ! $line{ 't_depth' } =~ /\d/ && 
+	    $line{ 't_ref_count' } =~ /\d/ && 
+	    $line{ 't_alt_count' } =~ /\d/ ) {
+	    
+	    $line{ 't_depth' } = $line{ 't_ref_count' } + $line{ 't_alt_count' };
+	    
+	}
+	if( ! $line{ 't_ref_count' } =~ /\d/ && 
+	    $line{ 't_depth' } =~ /\d/ && 
+	    $line{ 't_alt_count' } =~ /\d/ ) {
+	    $line{ 't_ref_count' } = $line{ 't_depth'} - $line{ 't_alt_count' };
+	    
+	}
+	if( ! $line{ 't_alt_count' } =~ /\d/  &&
+	    $line{ 't_depth' } =~ /\d/ && $line{ 't_ref_count' } =~ /\d/ ) {
+	    $line{ 't_alt_count' } = $line{ 't_depth' } - $line{ 't_ref_count' };
+	}
+	
+	# n_depth = n_ref_count + n_alt_count
+	if( ! $line{ 'n_depth' } =~  /\d/ && 
+	    $line{ 'n_ref_count' } =~ /\d/ && $line{ 'n_alt_count' } =~ /\d/) {
+	    $line{ 'n_depth' } = $line{ 'n_ref_count' } + $line{ 'n_alt_count' };
+	}
+	if( ! $line{ 'n_ref_count' } =~ /\d/ &&
+	    $line{ 'n_depth' } =~ /\d/ && $line{ 'n_alt_count' } =~ /\d/ ) {
+	    $line{ 'n_ref_count' } = $line{ 'n_depth'} - $line{ 'n_alt_count'  };
+	}
+
+	if( ! $line{ 'n_alt_count' } =~ /\d/ &&
+	    $line{ 'n_depth' } =~ /\d/ && $line{ 'n_ref_count' } =~ /\d/ ) {
+	    $line{ 'n_alt_count' } = $line{ 'n_depth' } - $line{ 'n_ref_count' };
+	}
+	
+	my $line = "";
+	
+	foreach( @header ) {
+	    
+	    $line .= $line{ $_ } || "";
+	    $line .= "\t";
+	    
+	}
+
+	chop $line;
+	print OUT $line,"\n";
+	$fix{ total }++;
+
+	$cnt++;
+	print $cnt,"\r" if( $options{ -d } == 1 );
+    }
+
+    print "\n" if( $options{ -d } == 1 );
+
+    my $yes = $fix{ 'yes' } || 0;
+    my $no_varClass = $fix{ 'no_varClass' } || 0;
+    my $no_entrez  = $fix{ 'no_entrez' } || 0;
+    my $no_total = $no_varClass + $no_entrez;
+    my $total = $fix{ 'total' } || 0;
+
+    debug( -id => "INFO",
+	   -val => "Fixing : refvar ( $yes / $total )" );
+
+    debug( -id => "INFO",
+	   -val => "Fixing : refvar - Ignored Total ( $no_total / $total )" );
+
+
+    debug( -id => "INFO",
+	   -val => "  -> Ignored Variant Classification ( $no_varClass / $total )" );
+
+    debug( -id => "INFO",
+	   -val => "  -> Ignored Entrez not found in cBio ( $no_entrez / $total )" );
+    
+    close( IN );
+    close( OUT );
+    close( NOTFIX );
+
+}
